b3a74187573a273d8940bd7fb67467c6e3ae8f53
rasant12 <rasante16@gmail.com>
2023-12-13 13:31:22 +0200

    random data plugin

---
+++ cypress/e2e/issue-create.cy.js
@@ -0,0 +1,77 @@
+describe('Issue create', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
+      //System will already open issue creating modal in beforeEach block  
+      cy.visit(url + '/board?modal-issue-create=true');
+    });
+  });
+
+  it.only(' Random Data Plugin Issue Creation', () => {
+    //System finds modal for creating issue and does next steps inside of it
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+
+      //open issue type dropdown and choose Task
+      cy.get('[data-testid="select:type"]').click()
+        .trigger('click');
+      cy.get('[data-testid="select:priority"]').click(),
+        cy.get('[data-testid="select-option:Low"]')
+          .trigger('click');
+
+
+      //Type value to description input field
+      cy.get('.ql-editor').type('Use_The_Random_Data_Plugin_For_Several_Words');
+
+      //Type value to title input field
+      //Order of filling in the fields is first description, then title on purpose
+      //Otherwise filling title first sometimes doesn't work due to web page implementation
+      cy.get('input[name="title"]').type('Use the random data plugin for a single word ');
+
+      //Select Baby Yoda from reporter dropdown
+      cy.get('[data-testid="select:userIds"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+
+
+      //Click on button "Create issue"
+      cy.get('button[type="submit"]').click();
+
+    });
+
+
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-story"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+        .should('have.length', '5')
+        .first()
+        .find('p')
+        .contains('Use the random data plugin for a single word');
+      //Assert that correct avatar and type icon are visible
+      cy.get('[data-testid="avatar:Baby Yoda"]').should('be.visible');
+      cy.get('[data-testid="icon:Story"]').should('not.exist');
+    });
+  });
+
+
+    
+
+    it('Should validate title is required field if missing', () => {
+      //System finds modal for creating issue and does next steps inside of it
+      cy.get('[data-testid="modal:issue-create"]').within(() => {
+        //Try to click create issue button without filling any data
+        cy.get('button[type="submit"]').click();
+
+      //Assert that correct error message is visible
+      cy.get('[data-testid="form-field:title"]').should('contain', 'This field is required');
+    });
+  });
+});
